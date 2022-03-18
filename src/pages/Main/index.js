74ec37200f1516843669d27647a653711c375401
Devpe007 <anddev007pe@gmail.com>
2022-03-18 07:29:39 -0300

    create the Main page structure

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,35 @@
 import React from "react";
 
-import { Title } from './styles';
+import { 
+  Container,
+  Form,
+  SubmitButton,
+} from './styles';
+
+import { 
+  FaGithub,
+  FaPlus, 
+} from 'react-icons/fa';
 
 function Main() {
   return (
-    <div>
-      <Title>MAIN</Title>
-    </div>
+    <Container>
+      <h1>
+        <FaGithub size={25} />
+        Meus Repositorios
+      </h1>
+
+      <Form onSubmit={() => {}} >
+        <input type="text" placeholder="Adicionar Repositorios" />
+
+        <SubmitButton>
+          <FaPlus
+            color="#ffffff"
+            size={14}
+          />
+        </SubmitButton>
+      </Form>
+    </Container>
   );
 };
 
