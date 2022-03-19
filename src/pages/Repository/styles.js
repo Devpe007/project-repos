c855ce038fc23352e42a7f1654351fe078457ad4
Devpe007 <anddev007pe@gmail.com>
2022-03-19 00:28:06 -0300

    create the stylization of the filter of the status of the repositories issues

--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -58,6 +58,25 @@ export const Owner = styled.header`
   }
 `;
 
+export const FilterList = styled.div`
+  margin: 15px 0;
+
+  button {
+    padding: 8px;
+    margin: 0 3px;
+
+    outline: 0;
+
+    border: 0;
+    border-radius: 4px;
+
+    &:nth-child(${props => props.active + 1}) {
+      background: #0071db;
+      color: #ffffff;
+    }
+  }
+`;
+
 export const BackButton = styled(Link)`
   border: 0;
 
